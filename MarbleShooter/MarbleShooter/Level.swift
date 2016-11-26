ba6b9e332642b9613af0c184523c2579c9613ac7
sambobkne <samuel.knepper@cerner.com>
2016-11-26 17:12:53 -0600

    Update Level.swift

--- MarbleShooter/MarbleShooter/Level.swift
+++ MarbleShooter/MarbleShooter/Level.swift
@@ -50,7 +50,17 @@ class Level {
         for row in 0..<NumRows {
             for column in 0..<NumColumns {
                 if slots[column, row] != nil {
-                    var ballType = BallType.random()
+                    //var ballType = BallType.random()
+                    //pick a random ball but make sure never to create chain of three or more
+                    var ballType: BallType
+                    repeat {
+                        ballType = BallType.random()
+                    } while (column >= 2 &&
+                        balls[column - 1, row]?.ballType == ballType &&
+                        balls[column - 2, row]?.ballType == ballType)
+                        || (row >= 2 &&
+                            balls[column, row - 1]?.ballType == ballType &&
+                            balls[column, row - 2]?.ballType == ballType)
                 
                     let ball = Ball(column: column, row: row, ballType: ballType)
                     balls[column, row] = ball
@@ -69,7 +79,126 @@ class Level {
         assert(row >= 0 && row < NumRows)
         return slots[column, row]
     }
+    
+    //check if there is a chain of same colored balls
+    private func hasChainAt(column: Int, row: Int) -> Bool {
+        let ballType = balls[column, row]!.ballType
+        
+        // Horizontal chain check
+        var horzLength = 1
+        
+        // Left
+        var i = column - 1
+        while i >= 0 && balls[i, row]?.ballType == ballType {
+            i -= 1
+            horzLength += 1
+        }
+        
+        // Right
+        i = column + 1
+        while i < NumColumns && balls[i, row]?.ballType == ballType {
+            i += 1
+            horzLength += 1
+        }
+        if horzLength >= 3 { return true }
+        
+        // Vertical chain check
+        var vertLength = 1
+        
+        // Down
+        i = row - 1
+        while i >= 0 && balls[column, i]?.ballType == ballType {
+            i -= 1
+            vertLength += 1
+        }
+        
+        // Up
+        i = row + 1
+        while i < NumRows && balls[column, i]?.ballType == ballType {
+            i += 1
+            vertLength += 1
+        }
+        return vertLength >= 3
+    }
 
+    
+    private func detectHorizontalMatches() -> Set<Chain> {
+        // 1
+        var set = Set<Chain>()
+        // 2
+        for row in 0..<NumRows {
+            var column = 0
+            while column < NumColumns-2 {
+                // 3
+                if let ball = balls[column, row] {
+                    let matchType = ball.ballType
+                    // 4
+                    if balls[column + 1, row]?.ballType == matchType &&
+                        balls[column + 2, row]?.ballType == matchType {
+                        // 5: there's a chain of at least 3 balls
+                        let chain = Chain(chainType: .horizontal)
+                        repeat {
+                            chain.add(ball: balls[column, row]!)
+                            column += 1
+                        } while column < NumColumns && balls[column, row]?.ballType == matchType
+                        
+                        set.insert(chain)
+                        continue
+                    }
+                }
+                // 6
+                column += 1
+            }
+        }
+        return set
+    }
+    
+    
+    
+    private func detectVerticalMatches() -> Set<Chain> {
+        var set = Set<Chain>()
+        
+        for column in 0..<NumColumns {
+            var row = 0
+            while row < NumRows-2 {
+                if let ball = balls[column, row] {
+                    let matchType = ball.ballType
+                    
+                    if balls[column, row + 1]?.ballType == matchType &&
+                        balls[column, row + 2]?.ballType == matchType {
+                        let chain = Chain(chainType: .vertical)
+                        repeat {
+                            chain.add(ball: balls[column, row]!)
+                            row += 1
+                        } while row < NumRows && balls[column, row]?.ballType == matchType
+                        
+                        set.insert(chain)
+                        continue
+                    }
+                }
+                row += 1
+            }
+        }
+        return set
+    }
+    
+    func removeMatches() -> Set<Chain> {
+        let horizontalChains = detectHorizontalMatches()
+        let verticalChains = detectVerticalMatches()
+        
+        removeBalls(chains: horizontalChains)
+        removeBalls(chains: verticalChains)
+        
+        return horizontalChains.union(verticalChains)
+    }
+    
+    private func removeBalls(chains: Set<Chain>) {
+        for chain in chains {
+            for ball in chain.balls {
+                balls[ball.column, ball.row] = nil
+            }
+        }
+    }
 }
 
 
