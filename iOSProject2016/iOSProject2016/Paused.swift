ae8c158a69bd2d04483b0e5f81e3107dbf7761fc
test commit <samuel-knepper@uiowa.edu>
2016-12-11 22:58:15 -0600

    removed unnecessary directories

--- iOSProject2016/iOSProject2016/Paused.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Paused.swift
-//  iOSProject2016
-//
-//  Created by Jonathan Light on 11/8/16.
-//  Copyright © 2016 Jonathan Light. All rights reserved.
-//
-
-import Foundation
--- iOSProject2016/iOSProject2016/Playing.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Playing.swift
-//  iOSProject2016
-//
-//  Created by Jonathan Light on 11/8/16.
-//  Copyright © 2016 Jonathan Light. All rights reserved.
-//
-
-import Foundation
